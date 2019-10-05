d626ed669501696f6031074df42816fef56867e8
xiaohuge <xiaohuge@xiaohugedeMBP.lan>
2019-10-05 11:13:00 +0800

    fix bug

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -13,9 +13,24 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read().decode())
+        db.commit()
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
 
 if __name__ == '__main__':
     app.run()
